157af133041c528b8766048b058023bed1616147
SchlickmannDamasceno <jschlickmanndamascen@vch.ca>
2018-10-31 11:53:51 -0700

    course C# and .NET

---
+++ Code/Chapter01/WelcomeDotNetUsingVSCODE/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace WelcomeDotNetUsingVSCODE
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello Welcome DotNet Core!");
+        }
+    }
+}
